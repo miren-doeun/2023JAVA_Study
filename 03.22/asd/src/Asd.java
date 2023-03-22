06e64ec28e24e5c918f02d15327d83de23ad2e6e
miren-doeun <111549068+miren-doeun@users.noreply.github.com>
2023-03-22 11:20:01 +0900

    new 03.22

--- 03.22/asd/src/Asd.java
+++ 03.22/asd/src/Asd.java
@@ -10,20 +10,26 @@ public class Asd {
 //		int[] a= new int [] {1,2,3,4,5,6,7,8,9,10};
 //		for(int i =0; i<a.length; i++)
 //			System.out.println("a[" + i +"] = " + a[i]);
-		int score[]= {1,2,3};
+		/*int score[]= {1,2,3,4,5};
 		int sum = 0;
 		double avg;
 		
-		Scanner sc= new Scanner(System.in);
+		Scanner sc  = new Scanner(System.in);
 		for(int i=0; i<5; i++) {
 			System.out.println("¼ºÀû ÀÔ·Â:");
-			int score1 = sc.nextInt();
-			sum+=score1;
+			score[i] = sc.nextInt();
+			sum+=score[i];
 		}
 		System.out.println("ÇÕ°è:"+sum);
-		avg=sum/5;
-		System.out.println("Æò±Õ:"+avg);
-
+		avg=sum/5.0;
+		System.out.println("Æò±Õ: 1"+avg);*/
+		
+		int a[][] = new int[2][3];
+		a[0][0]=1; a[0][1]=2; a[0][2]=3;
+		a[1][0]=10;a[1][1]=20; a[1][2]=30;
+		for(int i =0; i<a.length;i++)
+			for(int j =0; j<a[i].length;j++)
+				System.out.println(a[i][j]+ "\t");
 	}
 
 }
